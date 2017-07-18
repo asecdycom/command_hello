4a90c0fd155d67b5a94c12f37dbdcc79ece14334
asecdycom <asecdycom@gmail.com>
2017-07-18 16:58:53 +0900

    Test.java modified on GitHub

--- Test.java
+++ Test.java
@@ -2,6 +2,8 @@ public class Test{
 
 	public static void main(String argsp[]){
 
+		// For command line git tutorial
+		// GitHub modification
 		System.out.println("Hello World");
 		System.out.println("Tell Your World");
 		System.out.println("Tell his world");
