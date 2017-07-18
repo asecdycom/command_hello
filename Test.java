27647c70eb62e6e60920fc9774f33966688bc78f
asecdycom <asecdycom@neonexsoft.com>
2017-07-18 17:00:45 +0900

    Test.java modified on Local repository

--- Test.java
+++ Test.java
@@ -1,7 +1,9 @@
 public class Test{
 
 	public static void main(String argsp[]){
-
+		
+		// For command line git tutorial
+		// Local repository modification
 		System.out.println("Hello World");
 		System.out.println("Tell Your World");
 		System.out.println("Tell his world");
