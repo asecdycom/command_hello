3dcb29d08c09d6fd80c9cedf2b047b32e6f109fd
asecdycom <asecdycom@neonexsoft.com>
2017-07-18 15:40:05 +0900

    init

--- Test.java
+++ Test.java
@@ -3,9 +3,7 @@ public class Test{
 	public static void main(String argsp[]){
 
 		System.out.println("Hello World");
-		System.out.println("Modify");
 		System.out.println("Tell Your World");
-		System.out.println("Tell her world");
 	}
 
 }	
